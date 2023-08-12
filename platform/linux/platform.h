84fea012b8e910af092b0030d19458172788506e
daicmod <daicmod@gmail.com>
2023-08-13 01:48:24 +0900

    add step2

--- platform/linux/platform.h
+++ platform/linux/platform.h
@@ -4,19 +4,27 @@
 #include <stddef.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <signal.h>
+
+#define INTR_IRQ_BASE (SIGRTMIN+1)
+#define INTR_IRQ_SHARED 0x0001
+
+extern int intr_request_irq(unsigned int irq, int (*handler)(unsigned int irq, void *id), int flags, const char *name, void *dev);
+extern int intr_raise_irq(unsigned int irq);
+extern int intr_run(void);
+extern void intr_shutdown(void);
+extern int intr_init(void);
 
 /*
  * Memory
  */
 
-static inline void *
-memory_alloc(size_t size)
+static inline void *memory_alloc(size_t size)
 {
     return calloc(1, size);
 }
 
-static inline void
-memory_free(void *ptr)
+static inline void memory_free(void *ptr)
 {
     free(ptr);
 }
@@ -29,20 +37,17 @@ typedef pthread_mutex_t mutex_t;
 
 #define MUTEX_INITIALIZER PTHREAD_MUTEX_INITIALIZER
 
-static inline int
-mutex_init(mutex_t *mutex)
+static inline int mutex_init(mutex_t *mutex)
 {
     return pthread_mutex_init(mutex, NULL);
 }
 
-static inline int
-mutex_lock(mutex_t *mutex)
+static inline int mutex_lock(mutex_t *mutex)
 {
     return pthread_mutex_lock(mutex);
 }
 
-static inline int
-mutex_unlock(mutex_t *mutex)
+static inline int mutex_unlock(mutex_t *mutex)
 {
     return pthread_mutex_unlock(mutex);
 }
